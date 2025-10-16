4046ec7c302f762708f2e8f41b1b5ef85dcda482
Hannah O'Keeffe <hannahokeeffe@MacBook-Pro-139.local>
2025-10-16 12:02:52 +0100

    added .py

--- Decision_Tree_Class.py
+++ Decision_Tree_Class.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class DecisionTree:
 
     def __init__(self,depth=0):
@@ -8,6 +10,54 @@ class DecisionTree:
         self.depth = 0
         self.label = None
 
+
+    def calculate_entropy(self, subset):
+
+        len_dataset = len(subset[:, -1])
+        classes, counts = np.unique(subset[:, -1], return_counts=True)
+
+        entropy = 0
+        for i in range(len(classes)):
+            p = counts[i] / len_dataset
+            entropy -= p * np.log2(p)   # ← signe négatif + log base 2
+
+        return entropy
+
+    def find_split(self, data):
+        
+        best_attribute_matrix = np.zeros((data.shape[1]-1,3))
+
+        for attribute in range(data.shape[1]-1):
+            values = np.array(data[:, [attribute,-1]]) #get only the attribute and class columns
+            values = values[values[:,0].argsort()]
+            class_change = []
+
+            for i in range(len(values)-1): #check if class changes between two consecutive rows
+                if values[i,-1] != values[i+1,-1]:
+                    class_change.append(i)
+
+        # print(class_change)
+
+            remainder_array = np.zeros((len(class_change),2))
+            for indices in class_change : #for each index where the class changes, calculate the entropy of the split
+                remainder = len(values[:indices+1,:])/len(values) * self.calculate_entropy(values[:indices+1,:]) + len(values[indices+1:,:])/len(values) * self.calculate_entropy(values[indices+1:,:])
+                remainder_array[class_change.index(indices),:] = [int(indices), remainder]
+
+            best_split_index_reminder_array = np.argmin(remainder_array[:,1])
+
+            best_split_info = remainder_array[best_split_index_reminder_array,:]
+
+            threshold_value = (values[int(best_split_info[0]),0] + values[int(best_split_info[0]+1),0]) / 2
+
+        #  print("best split info is", best_split_info)
+
+            best_attribute_matrix[attribute,:] = [attribute, best_split_info[1],threshold_value]
+
+
+        best_attribute_index = np.argmin(best_attribute_matrix[:,1])
+
+        return best_attribute_matrix[best_attribute_index,[0,2]]
+    
     def train(self, dataset):
 
         #Base case
@@ -16,7 +66,7 @@ class DecisionTree:
             return
 
         else:
-            self.attribute, self.value = find_split(dataset)
+            self.attribute, self.value = self.find_split(dataset)
             self.attribute = int(self.attribute)
 
             #Split the dataset in two
@@ -42,3 +92,5 @@ class DecisionTree:
             return self.right_branch.predict(test)
         else:
             return self.left_branch.predict(test)
+        
+    
---
+++ decision_tree.py
@@ -0,0 +1,45 @@
+import numpy as np
+from numpy.random import default_rng
+from Decision_Tree_Class import DecisionTree
+
+
+
+
+def split_dataset(x, y, test_proportion, random_generator=default_rng()):
+
+    shuffled_indices = random_generator.permutation(len(x))
+    n_test = round(len(x) * test_proportion)
+    n_train = len(x) - n_test
+    x_train = x[shuffled_indices[:n_train]]
+    y_train = y[shuffled_indices[:n_train]]
+    x_test = x[shuffled_indices[n_train:]]
+    y_test = y[shuffled_indices[n_train:]]
+    train_dataset = np.column_stack((x_train, y_train))
+    test_dataset = np.column_stack((x_test, y_test))
+
+    return train_dataset, test_dataset
+
+
+
+def main():
+    print("Decision Tree Coursework")
+    
+    seed = 60012
+    rg = default_rng(seed)
+
+    #Load the data
+    clean_dataset = np.loadtxt('wifi_db/clean_dataset.txt')
+    noisy_data = np.loadtxt('wifi_db/noisy_dataset.txt')
+
+    raw_data = clean_dataset
+    
+    classes = np.unique(raw_data[:, -1])
+    x = raw_data[:, :-1]
+    y = raw_data[:, -1]
+
+    train_dataset, test_dataset = split_dataset(x, y, test_proportion=0.3, random_generator=rg)
+
+    dt = DecisionTree()
+    dt.train(train_dataset)
+
+main()
